7adf6e80e6d2071038b20e92137a50045857d61b
Bartosz Krol <adrilan.evenwood@gmail.com>
2016-06-29 23:33:35 +0200

    Add players page

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,68 +1,25 @@
 module Main exposing (..)
 
-import Html exposing (Html, div, text)
 import Html.App
+import Messages exposing (Msg)
+import Models exposing (Model, initialModel)
+import View exposing (view)
+import Update exposing (update)
 
-
--- MODEL
-
-
-type alias Model =
-    String
-
-
-init : ( Model, Cmd Msg )
+init : (Model, Cmd Msg)
 init =
-    ( "Hello", Cmd.none )
-
-
-
--- MESSAGES
-
-
-type Msg
-    = NoOp
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ text model ]
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        NoOp ->
-            ( model, Cmd.none )
-
-
-
--- SUBSCRIPTIONS
-
+  (initialModel , Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-    Sub.none
-
-
+  Sub.none
 
 -- MAIN
 
-
-main : Program Never
 main =
-    Html.App.program
-        { init = init
-        , view = view
-        , update = update
-        , subscriptions = subscriptions
-        }
+  Html.App.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
